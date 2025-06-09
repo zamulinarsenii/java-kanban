3f9fe450931bfc6652259133ff100126efdb1724
zamulinarsenii <114849263+zamulinarsenii@users.noreply.github.com>
2025-06-09 22:34:39 +0400

    Поменял принцип работы класса InMemoryTaskManager, добавил свой LinkedList

--- src/task/Epic.java
+++ src/task/Epic.java
@@ -5,19 +5,24 @@ import java.util.List;
 
 public class Epic extends Task {
     private List<Integer> subtasksId = new ArrayList<>();
+
     public List<Integer> getSubtasksId() {
         return subtasksId;
     }
+
     public void setSubtasksId(List<Integer> subtasksId) {
         this.subtasksId = subtasksId;
     }
+
     public Epic(String title, String description, int id) {
         super(title, description, id);
     }
+
     public Epic(Epic epic) {
         super(epic);
         this.subtasksId = new ArrayList<>(epic.subtasksId);
     }
+
     @Override
     public String toString() {
         String text = title + " | Status: " + status + ", id: " + id + "\n";
@@ -25,6 +30,7 @@ public class Epic extends Task {
         text += "Содержит подзадачи с id = " + subtasksId + "\n";
         return text;
     }
+
     @Override
     public Task copy() {
         return new Epic(this);
